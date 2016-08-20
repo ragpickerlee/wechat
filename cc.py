3379ae2a5ffe8dafdc3a131350dd1352db61d35e
leedb527 <lidaobin@cdrcb.com>
2016-08-20 14:28:46 +0800

    master branck cc.py

--- cc.py
+++ cc.py
@@ -2,3 +2,6 @@ alskdjfalskdjfa;lsdkfjalsdkfj
 modify on testing branch
 -----------------------------------
 modify on master branch
+
+-----------------------------
+modify on master branch ,second time
